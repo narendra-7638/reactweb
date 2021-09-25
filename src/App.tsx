f2f6c28a53dc62d57dcb0cd4672b0536492d3ac3
Narendra Vishwakarma <narendra.7638@gmail.com>
2021-09-25 17:58:49 +0000

    started

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Quiz</h1>
+      <p>Lets take a quiz guys.</p>
+    </div>
+  );
+}
+
+export default App;
